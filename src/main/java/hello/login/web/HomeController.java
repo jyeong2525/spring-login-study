fa6c5e700e224cca4693f05b9b60e94ea4371340
jyeong2525 <yhwan0226@gmail.com>
2024-07-31 22:07:00 +0900

    refactor: 서블릿 HTTP 세션1

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.login.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 @Controller
@@ -18,7 +20,6 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 
     private final MemberRepository memberRepository;
-    private final SessionManager sessionManager;
 
     //    @GetMapping("/")
     public String home() {
@@ -28,14 +29,20 @@ public class HomeController {
     @GetMapping("/")
     public String homeLogin(HttpServletRequest request, Model model) {
 
-        //세션 관리자에 저장된 회원 정보 조회
-        Member member = (Member) sessionManager.getSession(request);
-        if (member == null) {
+        //세션이 없으면 home
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember == null) {
             return "home";
         }
 
         //로그인
-        model.addAttribute("member", member);
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
