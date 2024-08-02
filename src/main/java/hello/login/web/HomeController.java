348a2badbda8f733ee3d22a3b35c7ec8c2597bdf
jyeong2525 <yhwan0226@gmail.com>
2024-08-02 15:30:22 +0900

    feat: ArgumentResolver

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.login.SessionConst;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
@@ -22,13 +23,8 @@ public class HomeController {
 
     private final MemberRepository memberRepository;
 
-    //    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-
     @GetMapping("/")
-    public String homeLogin(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+    public String homeLoginArgumentResolver(@Login Member loginMember, Model model) {
 
         //세션에 회원 데이터가 없으면 home
         if(loginMember == null) {
